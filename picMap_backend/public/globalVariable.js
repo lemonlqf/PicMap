bdad880ad26a86a2da7c573fb1b740ecd6a1eaab
吕奇峰 <1353041516@qq.com>
2025-01-25 21:18:59 +0800

    Featrue-1.0.0-初始化项目全局变量

---
+++ picMap_backend/public/globalVariable.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: Do not edit
+ * @Date: 2025-01-25 20:00:00
+ * @LastEditors: 吕奇峰 1353041516@qq.com
+ * @LastEditTime: 2025-01-25 20:12:36
+ * @FilePath: \Code\picMap_backend\public\globalVariable.js
+ * @Description:
+ */
+const globalVariables = {
+  imageFilePath: 'D:/PicMap/images/'
+}
+
+module.exports = globalVariables
